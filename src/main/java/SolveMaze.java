74e9ae4dac3a873fb66579575daf5a0266bcc532
Yuang <yuangz3@Illinois.edu>
2018-02-01 04:22:02 +0800

    Neo

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,6 +34,12 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+            while(maze.isFinished() == false) {
+                maze.turnLeft();
+                while(maze.canMove() == false)
+                    maze.turnRight();
+            }
+            maze.move();
             // Implement your maze solving algorithm here
         }
 
